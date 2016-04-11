701035b26f6139eb3fdcb0ff2177af06ab603afa
Roel Noort <roel.noort@gmail.com>
2016-04-11 07:06:51 +0200

    Added poweroff call to NAD

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function init() {
 	Homey.log("Started NAD");
 
 	Homey.manager('flow').on('action.select_input', function(callback, args) {
-		Homey.log("event music_on triggered args.input_channel=" + args.input_channel );
+		Homey.log("event select_input triggered args.input_channel=" + args.input_channel );
 
 		var h = Homey.manager("settings").get("host");
 		var p = Homey.manager("settings").get("port");
@@ -22,25 +22,35 @@ function init() {
 					Homey.log("successfully called switchnad service");
 					callback(null, true); // we've fired successfully
 				} else {
-					Homey.log("error calling switchnad service");
+					Homey.log("error calling switchinput service");
 					callback(null, false);
 				}
 			}
 		);
+	});
 
-/*
-		switch (args.input_channel) {
-			case "music":
-				Homey.log("sonos input");
-				break;
-			case "mediacenter":
-				Homey.log("mediacenter input");
-				break;
-			default:
-				Homey.log("other input");
-		}*/
+	Homey.manager('flow').on('action.power_off', function(callback, args) {
+		Homey.log("event power_off triggered");
 
-		//callback(null, true);
+		var h = Homey.manager("settings").get("host");
+		var p = Homey.manager("settings").get("port");
+		var url = h + ":" + p + "/poweroff/";
+
+		Homey.log("attempting to call [" + url + "]");
+
+		request.post(
+			url,
+			{},
+			function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					Homey.log("successfully called poweroff service");
+					callback(null, true); // we've fired successfully
+				} else {
+					Homey.log("error calling poweroff service");
+					callback(null, false);
+				}
+			}
+		);
 	});
 }
 
